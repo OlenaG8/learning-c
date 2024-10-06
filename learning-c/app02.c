79c156df8bfbc5d0224a4a2a4129419df8b62667
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-10-06 19:55:18 +0200

    Write an algorithm to reverse a number

--- learning-c/app02.c
+++ learning-c/app02.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-//In this example we use a while loop to reverse some numbers:
+//Write an algorithm to reverse a number:
 
 int main(void) {
-    
+
+    int myNum;
+    int reversedNum = 0;
+
+    printf("Enter a number you wish to reverse: ");
+    scanf("%d", &myNum);
+
+    while (myNum) {
+    reversedNum = reversedNum * 10 + myNum % 10;
+    myNum /= 10;
+    printf("%d \n", myNum);
+    }
+
+    printf("%d", reversedNum);
+    return 0;
+
 }
